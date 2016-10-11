ceeb97f87e3b4d02c73bf28d68a5b0b704ede805
xifanhenai <cj1252077975@163.com>
2016-10-12 00:17:33 +0800

    first edition

--- 026  Remove Duplicates from Sorted Array/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.cpp
+++ 026  Remove Duplicates from Sorted Array/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+/*
+class Solution {
+public:
+	Solution(vector<int>& n):nums(n){}
+    int removeDuplicates(vector<int>& nums) 
+	{
+        if (nums.size() == 0)
+			return 0;
+		else
+		{
+			unsigned int length = nums.size();
+			unsigned int index=0;
+			while(index++ < nums.size()-1)
+			{
+				if (nums[index] == nums[index-1])
+					length --;
+				
+			}
+			return length;
+		}
+    }
+private:
+	vector<int>& nums;
+};
+*/
 int main(void)
 {
-	cout << "hello world " << endl;
+	int a[] = {1,1,2};
+	vector<int> v(a,a+(sizeof(a)/sizeof(a[0])));
+	
+	vector<int>&v_temp=v;
+	Solution s(v_temp);
+	int result = s.removeDuplicates(v_temp);
 	return 0;
 }
